03239ead4e987e177eff0726289489786adcf537
RT-EGG <rt.egg.jp@gmail.com>
2021-12-21 21:38:26 +0900

    Add method.

--- RtCs.OpenTK/Utils/Extensions/MatrixExtensions.cs
+++ RtCs.OpenTK/Utils/Extensions/MatrixExtensions.cs
@@ -69,5 +69,25 @@ namespace RtCs.OpenGL
                 inMat[2, 3],
                 inMat[3, 3]
             };
+
+        public static void CopyToGLArray(this Matrix4x4 inMat, float[] inDst, int inOffset)
+        {
+            inDst[inOffset + 0] = inMat.m00;
+            inDst[inOffset + 1] = inMat.m10;
+            inDst[inOffset + 2] = inMat.m20;
+            inDst[inOffset + 3] = inMat.m30;
+            inDst[inOffset + 4] = inMat.m01;
+            inDst[inOffset + 5] = inMat.m11;
+            inDst[inOffset + 6] = inMat.m21;
+            inDst[inOffset + 7] = inMat.m31;
+            inDst[inOffset + 8] = inMat.m02;
+            inDst[inOffset + 9] = inMat.m12;
+            inDst[inOffset + 10] = inMat.m22;
+            inDst[inOffset + 11] = inMat.m32;
+            inDst[inOffset + 12] = inMat.m03;
+            inDst[inOffset + 13] = inMat.m13;
+            inDst[inOffset + 14] = inMat.m23;
+            inDst[inOffset + 15] = inMat.m33;
+        }
     }
 }
